2f018a537a46d6f9698c5ca9b311faa5a0023b92
Ajay Nair <ajnair@ucsd.edu>
2019-05-20 22:26:21 -0700

    added some more comments

--- HCTree.cpp
+++ HCTree.cpp
@@ -12,8 +12,11 @@
  * Function Prototype: HCTree(std::vector<std::pair<byte, int>> data);
  * Description: Constructor for HCTree object. Fills HCTree with the contents of
  *              the passed vector.
- * Parameters: data - vector of pairs where each element is made up of a symbol
- *                    and its frequency.
+ * Parameters: data - vector of pairs where each element contains the symbol of
+ *                    a supposed leaf node and the height at which it should be
+ *                    in the tree. contains the leaf nodes of an HCTree in the
+ *                    order of left-most leaf node first leading to right-most
+ *                    leaf node last.
  * Return Value: None
  */
 HCTree::HCTree(std::vector<std::pair<byte, int>> data) : 
@@ -25,7 +28,9 @@ HCTree::HCTree(std::vector<std::pair<byte, int>> data) :
 
   for(std::vector<std::pair<byte, int>>::iterator it = data.begin();
                                                      it != data.end(); it++) {
+    // keep building edges till depth of current leaf node data is reached.
     while(curDepth < (*it).second) {
+      // ensures we build, favoring building to the left before the right
       newNodePtr = (!cur->getZeroChild()) ? cur->getZeroChildPtr() :
                                             cur->getOneChildPtr();
       *newNodePtr = new HCNode(0, 0, nullptr, nullptr, cur);
@@ -33,9 +38,13 @@ HCTree::HCTree(std::vector<std::pair<byte, int>> data) :
       curDepth++;
     }
 
+    // once position of current leaf node has been found, assign its symbol to
+    //   the new leaf node
     cur->setSymbol((*it).first);
     done = false;
 
+    // traverse back up the tree until the root is reached or we found the next
+    //   unfinished branch of the tree
     while(cur != this->root && !done) {
       done = !cur->getParent()->getOneChild();
       cur = cur->getParent();
@@ -48,7 +57,8 @@ HCTree::HCTree(std::vector<std::pair<byte, int>> data) :
  * Function Name: build()
  * Function Prototype: build(const vector<int>& freqs);
  * Description: Builds this HCTree using the frequencies in passed vector. Each
- *              vector index is the integer value of an ASCII character.
+ *              vector index is the integer value of an ASCII character. Tree
+ *              is built using huffman encoding algorithm
  * Parameters: freqs - vector of frequencies, each element is the frequency of
  *                     its index value as a character
  * Return Value: None
@@ -56,6 +66,8 @@ HCTree::HCTree(std::vector<std::pair<byte, int>> data) :
 void HCTree::build(const vector<int>& freqs) {
   huffman_queue trees;
   HCNode* tree;
+
+  // populate the queue of leaf nodes
   for(unsigned int i = 0; i < freqs.size(); i++) {
     if(freqs[i] != 0) {
       tree = new HCNode(freqs[i], i);
@@ -67,22 +79,29 @@ void HCTree::build(const vector<int>& freqs) {
   HCNode* chZero;
   HCNode* chOne;
   HCNode* parent;
+
+  // keep processing queue of trees as long as there is more than 1 tree in the
+  //   queue
   while(trees.size() > 1) {
+    // pop two minimum frequency nodes off queue to combine into one tree
     chZero = trees.top();
     trees.pop();
 
     chOne = trees.top();
     trees.pop();
 
+    
     parent = new HCNode(chZero->getCount() + chOne->getCount(),
                                              chOne->getSymbol(), chZero, chOne);
 
     chZero->setParent(parent);
     chOne->setParent(parent);
 
+    // push newly created tree's root back into stack
     trees.push(parent);
   }
 
+  // the last remaining node is the root of the completed tree
   this->root = trees.top();
 }
 
@@ -150,6 +169,7 @@ int HCTree::decode(ifstream& in) const {
 int HCTree::decode(BitInputStream& in) const {
   HCNode* cur = this->root;
 
+  // traverse till leaf node reached
   while(cur->getZeroChild() && cur->getOneChild()) {
     cur = (in.readBit() == 0) ? cur->getZeroChild() : cur->getOneChild();
   }
@@ -244,6 +264,7 @@ void HCTree::encode(HCNode* node, BitOutputStream& out) const {
   if(!node)
     return;
   this->encode(node->getParent(), out);
+  // this check ensures we skip to next down in recursion stack when at root
   if(node->getParent()) {
     out.writeBit((node->getParent()->getZeroChild() == node) ? 0 : 1);
   }
@@ -256,7 +277,8 @@ void HCTree::encode(HCNode* node, BitOutputStream& out) const {
  * Description: Helper function for saveTree function. Recursively traverses to
  *              each leaf node in tree and writes its data to the output stream.
  *              Writes leaf node data in order from left-most leaf node, to
- *              right-most leaf node.
+ *              right-most leaf node. Data stored on each leaf-node includes
+ *              node's symbol and depth at which it is found in the HC-tree.
  * Parameters: node - pointer to current node in recursive traversal
  *             out - pointer to BitOutputStream containing output stream to
  *                   print leaf node data to
@@ -265,11 +287,12 @@ void HCTree::encode(HCNode* node, BitOutputStream& out) const {
  */
 void HCTree::saveHelper(HCNode* node, BitOutputStream* out, int d) {
   if(node) {
-    if(!(node->getZeroChild() || node->getOneChild())) {
+    if(!(node->getZeroChild() || node->getOneChild())) { // found leaf node
+      // write leaf-node's symbol and depth in tree to output stream
       out->writeByte(node->getSymbol());
       out->writeByte(d);
     }
-    else {
+    else { // traverse recursively, favoring left traversal before right
       this->saveHelper(node->getZeroChild(), out, d + 1);
       this->saveHelper(node->getOneChild(), out, d + 1);
     }
